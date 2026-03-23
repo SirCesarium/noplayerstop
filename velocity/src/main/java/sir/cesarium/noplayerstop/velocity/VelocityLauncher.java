bfcdd31da86ae0a7666bc2dc62f902413e24b413
SirCesarium <cesarium3600@gmail.com>
2026-03-22 21:36:52 -0400

    feat(velocity): implement SLF4J to JUL logger bridge

--- velocity/src/main/java/sir/cesarium/noplayerstop/velocity/VelocityLauncher.java
+++ velocity/src/main/java/sir/cesarium/noplayerstop/velocity/VelocityLauncher.java
@@ -1,19 +1,31 @@
 package sir.cesarium.noplayerstop.velocity;
 
 import com.velocitypowered.api.proxy.ProxyServer;
+import org.slf4j.LoggerFactory;
 import sir.cesarium.noplayerstop.api.IPlatformLauncher;
 
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 public class VelocityLauncher implements IPlatformLauncher {
     private final ProxyServer server;
-    private final org.slf4j.Logger logger;
+    private final org.slf4j.Logger SLF4J;
     private String lastPlayerName = "None";
 
     public VelocityLauncher(ProxyServer server, org.slf4j.Logger logger) {
         this.server = server;
-        this.logger = logger;
+        this.SLF4J = logger;
     }
+
+    private final java.util.logging.Logger loggerBridge = new java.util.logging.Logger("NoPlayerStop", null) {
+        @Override
+        public void log(LogRecord record) {
+            if (record.getLevel() == Level.SEVERE) SLF4J.error(record.getMessage());
+            else if (record.getLevel() == Level.WARNING) SLF4J.warn(record.getMessage());
+            else SLF4J.info(record.getMessage());
+        }
+    };
 
     @Override
     public int getPlayerCount() {
@@ -36,6 +48,6 @@ public class VelocityLauncher implements IPlatformLauncher {
 
     @Override
     public Logger getLogger() {
-        return Logger.getLogger("NoPlayerStop");
+        return loggerBridge;
     }
 }
